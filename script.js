3732cddcd5a602da1e7969da1538baedfca8a397
vKnitOn <hino.0@outlook.jp>
2025-07-19 13:08:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -118,6 +118,8 @@ function toComma(num){
 function convertNumber(inputStr, outputFormat) {
     const num = parseNumber(inputStr);
     if (isNaN(num)) return '無効な数値です';
+    if(num < 0) return '正の値を入力してください。';
+    num = Math.floor(num)
 
     switch (outputFormat) {
         case 'exp': return toExponential(num);
